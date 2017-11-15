3e891281f5885c6b59733352388ab16e2de50ec8
Algo Square <algosquare.contact@gmail.com>
2017-11-15 17:58:47 +0100

    fix .active

--- js/all.js
+++ js/all.js
@@ -3,25 +3,50 @@ $(document).ready(function() {
 	var out = true;
 	var hide = true;
 	var hide = true;
+	var grey = false;
 	var myTop = $("#main-container").offset().top;
+	var myBottom = $("#main-wrapper").offset().top;
+	console.log("myBottom = " +myBottom);
+	console.log("myTop = " +myTop);
+	console.log("window = " +$(window).scrollTop());
 	$(window).scroll(function() {
-		if($(window).scrollTop() > myTop && hide == true)
+		if($(window).scrollTop() * 2 > myTop && hide == true)
 		{
 
 			$("#bars").css("transform", "translateX(30vw)");
 			$("#bars #burg").css("transform", "rotate(0deg)");
 			hide = false;
 		}
-		else if($(window).scrollTop() <= myTop) {
+		else if($(window).scrollTop() * 2 <= myTop) {
 			$("#bars").css("transform", "translateX(-30vw)");
 			$("#bars #burg").css("transform", "rotate(-360deg)");
 			hide = true;
 		}
+		if($(window).scrollTop() > myBottom) {
+			$("#bars #burg").css("background-color", "#eada33");
+			$("#nav-bis").css("background-color", "#eada33");
+			$("#bars .active-g").addClass("active");
+			$("#bars .active-g").removeClass("active-g");
+			grey = false;
+		}
+		else {
+			$("#bars #burg").css("background-color", "#888888");
+			$("#nav-bis").css("background-color", "#888888");
+			$("#bars .active").addClass("active-g");
+			$("#bars .active").removeClass("active");
+			grey = true;
+		}
 	});
 	$("#bars #burg").hover(function() {
-		$("#bars #burg").css("background-color", "#efb62e");
+		if(grey == false)
+			$("#bars #burg").css("background-color", "#efb62e");
+		else
+			$("#bars #burg").css("background-color", "#656363");
 	}, function() {
-		$("#bars #burg").css("background-color", "#eada33");
+		if(grey == false)
+			$("#bars #burg").css("background-color", "#eada33");
+		else
+			$("#bars #burg").css("background-color", "#888888");
 	});
 	$("#bars #burg").click(function() {
 		if(out == true) {
